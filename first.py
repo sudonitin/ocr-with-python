eccae30eed11ea99c883c4660ad70231f3a2a25d
unknown <globefire>
2018-01-17 15:04:50 +0530

    hii

---
+++ first.py
@@ -0,0 +1,11 @@
+print("Enter 2 numbers")
+print("enter first number, a = ")
+a = int(input())
+print("Enter second number, b = ")
+b = int(input())
+print("Nos swapped are ")
+temp = a
+a = b
+b = temp
+print("a = ", a)
+print("b = ", b)
